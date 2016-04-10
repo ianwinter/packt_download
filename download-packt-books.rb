6aaee0e39fb4afc3f5711e825ece953fd699c46f
Ian Winter <ianwinter@gmail.com>
2016-04-10 21:46:14 +0100

    Change downloading messages, change title searching

--- download-packt-books.rb
+++ download-packt-books.rb
@@ -33,7 +33,7 @@ download_base = config["downloadpath"]
 book_titles = my_books_page.search(".//div[@class='title']")
 book_titles.each do |b|
   title = b.children.text.lstrip.rstrip
-  book_block = my_books_page.search(".//div[@title=\"#{title}\" and @class='product-line unseen']")
+  book_block = my_books_page.search(".//div[contains(@title, \"#{title}\") and @class='product-line unseen']")
   # map book downloads to title
   book_block.each do |bk|
    book_map[bk.attributes['nid'].value] = title.sub(" [eBook]", "")
@@ -58,12 +58,11 @@ my_books_page.links.each do |link|
     end
     file_name = file_name.gsub(" ","_")
     file_name = file_name.gsub("/","-")
-    puts file_name
-
     begin
       if File.exist?(download_base + file_name)
-        puts "  => already downloaded"
+        puts "[EXISTS] #{file_name}"
       else
+        puts "[DOWNLOADING] #{file_name}"
         agent.download(full_link, download_base + file_name)
       end
     rescue Mechanize::ResponseReadError => e
